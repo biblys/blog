5617400cab7ff7531d91bbd351d62299b46a8efd
Clément Latzarus <c@ltzr.io>
2023-08-30 15:02:48 +0200

    feat: add umami tracker

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          async
+          src="https://analytics.umami.is/script.js"
+          data-website-id="984d1210-a376-4349-aa2f-83fab030a249"
+        >
+        </script>
+      </Head>
       <body>
         <Main />
         <NextScript />
