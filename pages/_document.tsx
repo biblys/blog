8509535cf0030d3223962afbea9884481e0ebb1d
Clément Latzarus <c@ltzr.io>
2023-08-30 23:08:10 +0200

    feat: add google search console validation code

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,7 @@ export default function Document() {
           data-website-id="984d1210-a376-4349-aa2f-83fab030a249"
         >
         </script>
+        <meta name="google-site-verification" content="05Va7GfST4wDObPj7FVvSPulwafGcO53jC1f3L-Ixig" />
       </Head>
       <body>
         <Main />
