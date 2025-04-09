e018caff4199d6d920788fb4d8d80d33eb344bde
Clément Latzarus <c@ltzr.io>
2025-04-09 11:52:12 +0200

    feat: improve displaying dates

--- src/lib/utils/dateFormat.ts
+++ src/lib/utils/dateFormat.ts
@@ -3,7 +3,7 @@ import { fr } from "date-fns/locale";
 
 const dateFormat = (
   date: Date | string,
-  pattern: string = "dd MMMM yyyy",
+  pattern: string = "d MMMM yyyy",
 ): string => {
   const dateObj = new Date(date);
   const output = format(dateObj, pattern, { locale: fr });
